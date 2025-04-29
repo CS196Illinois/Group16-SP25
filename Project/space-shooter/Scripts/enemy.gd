9d8456744410e30ee8330baf75e597b539ca6688
LuyaoWang12 <luyao6@illinois.edu>
2025-04-29 00:41:30 -0500

    Space shooter added

---
+++ Project/space-shooter/Scripts/enemy.gd
@@ -0,0 +1,30 @@
+class_name Enemy extends Area2D
+
+
+signal killed
+signal hit
+
+@export var speed = 150
+@export var hp = 3
+
+func _physics_process(delta):
+	global_position.y += speed * delta
+
+func die():
+	queue_free()
+
+func _on_body_entered(body) -> void:
+	if body is Player:
+		body.die()
+		die()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+func take_damage():
+	hit.emit()
+	hp -= 1
+	if hp <= 0:
+		killed.emit()
+		die()
+	
