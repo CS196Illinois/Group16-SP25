68929332c158db645ee174a8e13d9888bd1b9426
KeshatM <keshat.mehra@gmail.com>
2025-05-03 04:00:19 -0500

    fixed space-shooter score bug

--- Project/space-shooter/Scripts/laser.gd
+++ Project/space-shooter/Scripts/laser.gd
@@ -10,7 +10,6 @@ func _ready():
 	if not is_connected("area_entered", Callable(self, "_on_area_entered")):
 		area_entered.connect(Callable(self, "_on_area_entered"))
 
-
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
 	
